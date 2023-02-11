24c8bd7917e9013109d292ae9f04f59dee2cb92d
taniyow <markcmtan@gmail.com>
2023-02-11 23:59:07 +0800

    Config: Installed and configured flowbite-react plugin components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./public/**/*.html",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        './node_modules/flowbite-react/**/*.js',
+        './src/**/*.{js,ts,jsx,tsx}', 
+        './public/**/*.html',
+    ],
+    plugins: [require('flowbite/plugin')],
+    theme: {
+        extend: {},
+    },
 }
