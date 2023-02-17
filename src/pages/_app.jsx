50127e099a56c6bc3eb1347cf2e010f47b1b294c
taniyow <markcmtan@gmail.com>
2023-02-18 04:10:23 +0800

    Feat: BE - Metamask Authentication

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,11 +1,14 @@
 import '@/styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from './_theme'
+import MetamaskProvider from '@/context/Metamask' 
 
 export default function App({ Component, pageProps }) {
     return (
         <ChakraProvider theme={theme}>
-            <Component {...pageProps} />
+            <MetamaskProvider>
+                <Component {...pageProps} />
+            </MetamaskProvider>
         </ChakraProvider>
     )
 }
