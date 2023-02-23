c2e07a87da80446d425c0b6aa73cca4f5bf3f3e6
taniyow <markcmtan@gmail.com>
2023-02-23 18:30:27 +0800

    Fix: FE - Add Toast for Validation on Metamask Authentication

--- src/context/Metamask.jsx
+++ src/context/Metamask.jsx
@@ -1,23 +1,46 @@
 import { createContext, useEffect, useState } from 'react'
+import { truncateAddress } from '@/utilities/address.utils';
+import { useToast } from '@chakra-ui/react';
 
 export const MetamaskContext = createContext()
 
 const { ethereum } = typeof window !== 'undefined' ? window : {}
 
 const MetamaskProvider = ({ children }) => {
+    const [disconnected, setDisconnected] = useState(false)
     const [account, setAccount] = useState('')
-    const [error, setError] = useState('')
+    
+    const toast = useToast()
+    const successMessage = (value) => {
+        toast({
+            title: 'Success',
+            description: `Connected to ${value}`,
+            status: 'success',
+            duration: 3000,
+            isClosable: true,
+            position: 'bottom-right',
+        })
+    }
+    const errorMessage = (value) => {
+        toast({
+            title: 'Error',
+            description: `${value}`,
+            status: 'error',
+            duration: 3000,
+            isClosable: true,
+            position: 'bottom-right',
+        })
+    }
 
-    const checkEthereumExists = () => {
+    const isMetamaskInstalled = () => {
         if (!ethereum) {
-            setError('Please Install MetaMask.')
+            errorMessage('Please Install MetaMask.')
             return false
         }
         return true
     }
 
-    const getConnectedAccounts = async () => {
-        initErrorState()
+    const isMetamaskConnected = async () => {
         try {
             const accounts = await ethereum.request({
                 method: 'eth_accounts',
@@ -25,48 +48,44 @@ const MetamaskProvider = ({ children }) => {
             console.log(accounts)
             setAccount(accounts[0])
         } catch (err) {
-            setError(err.message)
+            errorMessage(err.message)
         }
     }
 
     const connectWallet = async () => {
-        initErrorState()
-        if (checkEthereumExists()) {
+        if (isMetamaskInstalled()) {
             try {
                 const accounts = await ethereum.request({
                     method: 'eth_requestAccounts',
                 })
                 console.log(accounts)
                 setAccount(accounts[0])
+                successMessage(truncateAddress(accounts[0])) 
             } catch (err) {
-                setError(err.message)
+                errorMessage(err.message)
             }
         }
     }
 
-    const initErrorState = async () => {
-        setError('')
-    }
-
-    const disconnect = async () => {
-        setError('')
+    const disconnect = () => {
         setAccount('')
+        setDisconnected(true)
     }
 
     useEffect(() => {
-        if (checkEthereumExists()) {
-            ethereum.on('accountsChanged', getConnectedAccounts)
-            getConnectedAccounts()
+        if (isMetamaskInstalled()) {
+            ethereum.on('accountsChanged', isMetamaskConnected)
+            isMetamaskConnected()
         }
         return () => {
-            if (checkEthereumExists()) {
-                ethereum.removeListener('accountsChanged', getConnectedAccounts)
+            if (isMetamaskInstalled()) {
+                ethereum.removeListener('accountsChanged', isMetamaskConnected)
             }
         }
     }, [])
 
     return (
-        <MetamaskContext.Provider value={{ account, connectWallet, initErrorState, error, disconnect }}>
+        <MetamaskContext.Provider value={{account, connectWallet, disconnect}}>
             {children}
         </MetamaskContext.Provider>
     )
