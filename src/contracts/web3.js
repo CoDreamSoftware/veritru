7636b568efdc0973e1da3e30bbf9d199f746dfc8
taniyow <markcmtan@gmail.com>
2023-03-13 08:03:44 +0800

    Feat: BC - Refactored Fetch Articles from Blockchain Contract

--- src/contracts/web3.js
+++ src/contracts/web3.js
@@ -8,7 +8,8 @@ if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
     // We are in the browser and metamask is running.
     window.ethereum.request({ method: 'eth_requestAccounts' })
     web3 = new Web3(window.ethereum)
-} else {
+} 
+else {
     // We are on the server *OR* the user is not running metamask
     const provider = new Web3.providers.HttpProvider(
         `https://goerli.infura.io/v3/${infuraKey}`
