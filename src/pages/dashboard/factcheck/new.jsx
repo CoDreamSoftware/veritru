2136a63a563aa22e9fdd43fb0e8084c1f3328159
taniyow <markcmtan@gmail.com>
2023-03-19 04:50:44 +0800

    Minor Fix

--- src/pages/dashboard/factcheck/new.jsx
+++ src/pages/dashboard/factcheck/new.jsx
@@ -314,7 +314,7 @@ export default function NewArticle() {
                                 )}
                             </button>
                             <Link 
-                                href="/"
+                                href="/dashboard"
                                 type="button" 
                                 className="text-gray-600 bg-white border border-gray-300 focus:outline-none hover:bg-gray-200 focus:ring-2 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mx-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
                                 Cancel
