48e2a787d9d758800fcbcf99aa4ce3a8610ab7dc
taniyow <markcmtan@gmail.com>
2023-04-23 10:42:19 +0800

    Fix: Minor adjustments

--- src/pages/guides.jsx
+++ src/pages/guides.jsx
@@ -6,8 +6,8 @@ export default function Guides() {
 
     return (
         <Layout>
-            <div className="py-24">
-                <Tabs variant='soft-rounded' colorScheme='blue'>
+            <div className="py-24 h-full">
+                <Tabs variant="soft-rounded" colorScheme="blue">
                     <TabList>
                         <Tab>Metamask Setup</Tab>
                         <Tab>Goerli Faucet</Tab>
@@ -17,11 +17,10 @@ export default function Guides() {
                             <Image src="/assets/png/veritru-user-guide-metamask-1.png" className="w-full" alt="VeriTru User Guide 1" width="1200" height="720" unoptimized priority />
                         </TabPanel>
                         <TabPanel>
-                        <Image src="/assets/png/veritru-user-guide-metamask-2.png" className="w-full" alt="VeriTru User Guide 2" width="1200" height="720" unoptimized priority />
+                            <Image src="/assets/png/veritru-user-guide-metamask-2.png" className="w-full" alt="VeriTru User Guide 2" width="1200" height="720" unoptimized priority />
                         </TabPanel>
                     </TabPanels>
                 </Tabs>
-                
             </div>
         </Layout>
     )
