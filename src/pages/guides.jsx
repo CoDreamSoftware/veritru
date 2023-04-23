aecb344d404cf7d895b10f911d7012b8a13a2e0a
taniyow <markcmtan@gmail.com>
2023-04-23 10:35:18 +0800

    Feat: Updated User Guides and used image method instead of pdf

--- src/pages/guides.jsx
+++ src/pages/guides.jsx
@@ -1,11 +1,27 @@
 import Layout from "@/components/Layout"
-import DisplayPDF from "@/components/DisplayPDF"
+import Image from 'next/image'
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 
 export default function Guides() {
+
     return (
         <Layout>
             <div className="py-24">
-                <DisplayPDF cid="QmP1ku6BkdHnSwmVKHKRRTN6AyH4Vi6iirfjAsuvLviTq3"/>
+                <Tabs variant='soft-rounded' colorScheme='blue'>
+                    <TabList>
+                        <Tab>Metamask Setup</Tab>
+                        <Tab>Goerli Faucet</Tab>
+                    </TabList>
+                    <TabPanels>
+                        <TabPanel>
+                            <Image src="/assets/png/veritru-user-guide-metamask-1.png" className="w-full" alt="VeriTru User Guide 1" width="1200" height="720" unoptimized priority />
+                        </TabPanel>
+                        <TabPanel>
+                        <Image src="/assets/png/veritru-user-guide-metamask-2.png" className="w-full" alt="VeriTru User Guide 2" width="1200" height="720" unoptimized priority />
+                        </TabPanel>
+                    </TabPanels>
+                </Tabs>
+                
             </div>
         </Layout>
     )
